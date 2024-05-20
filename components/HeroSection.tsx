dcc852bf1f1f8c18cd95e7a2ea515569685d73f2
Federico <fedemarti0107@gmail.com>
2024-05-20 14:26:54 -0300

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
           />
         </div>
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hola!, soy Fede.</h1>
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hola, soy Fede!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
             Soy{" "}
             <span className="font-semibold text-teal-600">
